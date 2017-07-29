527f396ffdf2aad2a76ba458860f3ec014428e1a
vardhan <vardhan1529@gmail.com>
2017-07-29 19:55:57 +0530

    Added Function to calculate no of working day i.e exclude saturday and sunday from days

--- CSharpLibrary/CSharpLibrary/Program.cs
+++ CSharpLibrary/CSharpLibrary/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpLibrary.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,22 @@ namespace CSharpLibrary
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                TestCalculateWorkingDays();
+            }
+           Console.ReadKey();
+        }
+
+        private static void TestCalculateWorkingDays()
+        {
+            Console.WriteLine("Calculating the no of working days from current date to targeted date\n");
+            Console.Write("Enter start date:");
+            var startDate = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Enter the target date:");
+            var targetDate = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Start Date: {0}      End Date:{1}", startDate.ToString("dd-MM-yyyy"), targetDate.ToString("dd-MM-yyyy"));
+            Console.WriteLine("Total Working days: {0}", DateUtils.CalculateWorkingDays(startDate, targetDate ));
         }
     }
 }
---
+++ CSharpLibrary/CSharpLibrary/Utility/DateUtils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary.Utility
+{
+    public class DateUtils
+    {
+        public static int CalculateWorkingDays(DateTime d1, DateTime d2)
+        {
+            DateTime startDate;
+            DateTime endDate;
+            if(d1 > d2)
+            {
+                startDate = d2;
+                endDate = d1;
+            }
+            else
+            {
+                startDate = d1;
+                endDate = d2;
+            }
+
+            var startDay = startDate.DayOfWeek;
+            var daysDiff = (endDate - startDate).Days + 1; 
+           
+            if(daysDiff < 9)
+            {
+                var dayCount = 0;
+                for(var i = 0; i < daysDiff; i++)
+                {
+                    var dayOfWeek = startDate.AddDays(i).DayOfWeek;
+                    if(dayOfWeek != DayOfWeek.Saturday && dayOfWeek != DayOfWeek.Sunday)
+                    {
+                        dayCount++;
+                    }
+                }
+
+                return dayCount;
+            }
+
+            var initialCutoff = GetInitialCutOff(startDay);
+            var modifiedInitialDate = startDate.AddDays(initialCutoff);
+           
+            var totalDiffDays = (endDate - modifiedInitialDate).Days + 1;
+            var weeks = totalDiffDays / 7;
+            var finalCutoff = totalDiffDays % 7;
+            if(finalCutoff > 5)
+            {
+                finalCutoff = 5;
+            }
+
+            if(!(startDay == DayOfWeek.Sunday || startDay == DayOfWeek.Saturday))
+            {
+                initialCutoff -= 2;
+            }
+            else
+            {
+                initialCutoff = 0;
+            }
+
+            return initialCutoff + (weeks * 5) + finalCutoff;
+        }
+
+        private static int GetInitialCutOff(DayOfWeek day)
+        {
+            switch(day)
+            {
+                case DayOfWeek.Sunday: return 1;
+                case DayOfWeek.Saturday: return 2;
+                case DayOfWeek.Friday: return 3;
+                case DayOfWeek.Thursday: return 4;
+                case DayOfWeek.Wednesday: return 5;
+                case DayOfWeek.Tuesday: return 6;
+                case DayOfWeek.Monday: return 7;
+                default: return 0;
+            }
+        }
+    }
+}
