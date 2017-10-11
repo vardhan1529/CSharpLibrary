15761edb34b33209516a10dedd3774357602a867
Jayavardhan <vardhan1529@gmail.com>
2017-10-11 19:06:26 +0530

    Simple Execution time calculation for performance check

--- CSharpLibrary/CSharpLibrary/Program.cs
+++ CSharpLibrary/CSharpLibrary/Program.cs
@@ -49,7 +49,7 @@ namespace CSharpLibrary
             t = "Static field";
         }
 
-        public ST():this(string.Empty)
+        public ST() : this(string.Empty)
         {
         }
 
@@ -72,11 +72,10 @@ namespace CSharpLibrary
         {
             Debug.WriteLine("Debug Information-Product Starting ");
             #if true
-                Console.WriteLine("test");
+            Console.WriteLine("test");
             #endif
 
             Console.WriteLine("From Service");
-            Console.ReadKey();
         }
 
         public static void DynamicObjectCreation()
@@ -99,8 +98,8 @@ namespace CSharpLibrary
         }
         public static void TestFtpDownload()
         {
-            Ftp f = new Ftp("ftp://ftp.uconn.edu/48_hour/", "anonymous","");
-            f.GetResponse("SampleInfo.json");
+            Ftp f = new Ftp("ftp://ftp.uconn.edu/48_hour/", "anonymous", "");
+            f.GetResponse("Sample1");
         }
 
         public static void TestFtpUpload()
@@ -117,13 +116,125 @@ namespace CSharpLibrary
 
         public List<string> GetCountries()
         {
-                var countries = countryRepo.GetCountryNames();
-                for (var i = 0; i < countries.Count; i++)
-                {
-                    countries[i] = countries[i].ToLower();
-                }
+            var countries = countryRepo.GetCountryNames();
+            for (var i = 0; i < countries.Count; i++)
+            {
+                countries[i] = countries[i].ToLower();
+            }
             return countries;
         }
+
+        #region Time check
+        public struct Point
+        {
+            public int X;
+            public int Y;
+            public string S;
+        }
+
+        public struct PointA
+        {
+            public int X;
+            public int Y;
+            public int Z;
+            public int A;
+            public string S;
+        }
+
+        public class PointC
+        {
+            public int X;
+            public int Y;
+            public string S;
+        }
+
+        public static void TestStruct()
+        {
+            Point p1 = new Point();
+            p1.X = 5;
+            p1.Y = 6;
+            p1.S = "T";
+            Point p2 = new Point();
+            p2.X = 5;
+            p2.Y = 6;
+            p2.S = "S";
+        }
+
+        public static void TestClass()
+        {
+            PointC p1 = new PointC();
+            p1.X = 5;
+            p1.Y = 6;
+            p1.S = "T";
+            PointC p2 = new PointC();
+            p2.X = 5;
+            p2.Y = 6;
+            p2.S = "S";
+        }
+
+        public static void TestStaticParam(Point p)
+        {
+            var x = p;
+        }
+
+        public static void TestClassParam(PointC p)
+        {
+            var x = p;
+        }
+
+        public static void TestStaticMoreParam(PointA p)
+        {
+            var x = p;
+        }
+        public static void TimeCheckForMethodExecution()
+        {
+            //clean Garbage
+            GC.Collect();
+
+            //wait for the finalizer queue to empty
+            GC.WaitForPendingFinalizers();
+
+            //clean Garbage
+            GC.Collect();
+
+            var t2 = SimplePerfomanceCheck.PerformTest(TestClass);
+            var t1 = SimplePerfomanceCheck.PerformTest(TestStruct);
+
+
+            var p3 = new PointA();
+            p3.S = "SS"; p3.X = 4; p3.Y = 45; p3.Z = 456; p3.A = 789;
+
+            //0.1504
+            Stopwatch s1 = Stopwatch.StartNew();
+            TestStaticMoreParam(p3);
+            s1.Stop();
+            var staticMoreParam = s1.Elapsed.TotalMilliseconds;
+            Console.WriteLine($"Time taken to call a method with struct parameter containing 4 int and 1 string is {staticMoreParam}");
+
+            var p2 = new PointC() { S = "FF", X = 4, Y = 45 };
+            //0.248
+            Stopwatch s3 = Stopwatch.StartNew();
+            TestClassParam(p2);
+            s3.Stop();
+            var classParam = s3.Elapsed.TotalMilliseconds;
+            Console.WriteLine($"Time taken to call a method with refernece type parameter is {classParam}");
+
+            var p1 = new Point();
+            p1.S = "SS"; p1.X = 4; p1.Y = 45;
+
+            //0.1518
+            Stopwatch s2 = Stopwatch.StartNew();
+            TestStaticParam(p1);
+            s2.Stop();
+            var staticLessParam = s2.Elapsed.TotalMilliseconds;
+            Console.WriteLine($"Time taken to call a method with struct parameter containing 2 int and 1 string is {staticLessParam}");
+
+            var t5 = SimplePerfomanceCheck.PerformTest(TestStaticMoreParam, p3);
+            var t3 = SimplePerfomanceCheck.PerformTest(TestStaticParam, p1);
+            var t4 = SimplePerfomanceCheck.PerformTest(TestClassParam, p2);
+        }
+
+        #endregion
 
         private static string GetAbsolutePathOfAFile(string relativePath)
         {
---
+++ CSharpLibrary/CSharpLibrary/Utility/SimplePerfomanceCheck.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CSharpLibrary.Utility
+{
+    static class SimplePerfomanceCheck
+    {
+        public static double PerformTest(Action function)
+        {
+            var watch = new TimeCalculator();
+            watch.Start();
+            function();
+            watch.Stop();
+            return watch.GetElapsedTime().TotalMilliseconds;
+        }
+
+        public static double PerformTest<T>(Action<T> function, T param)
+        {
+            var watch = new TimeCalculator();
+            watch.Start();
+            function(param);
+            watch.Stop();
+            return watch.GetElapsedTime().TotalMilliseconds;
+        }
+
+        public static double PerformTest(Action function, int iterations)
+        {
+            var watch = new TimeCalculator();
+            watch.Start();
+            for (var i = 0; i < iterations; i++)
+            {
+                function();
+            }
+            watch.Stop();
+            return watch.GetElapsedTime().TotalMilliseconds;
+        }
+
+        public static double PerformNormalisedTest(Action function, int iterations)
+        {
+            var watch = new TimeCalculator();
+            double[] data = new double[4];
+            for (var i = 0; i < 4; i++)
+            {
+                watch.Reset();
+                watch.Start();
+                for (var j = 0; j < iterations; j++)
+                {
+                    function();
+                }
+                watch.Stop();
+                data[i] = watch.GetElapsedTime().TotalMilliseconds;
+            }
+
+            return data.CalculateMean();
+        }
+
+        private static double CalculateMean(this ICollection<double> data)
+        {
+            var mean = data.Average();
+            double standardDeviation = 0;
+            foreach (var d in data)
+            {
+                standardDeviation += Math.Pow((d - mean), 2);
+            }
+
+            standardDeviation = Math.Sqrt(standardDeviation / data.Count);
+
+            return data.Where(m => Math.Abs(m - mean) <= standardDeviation).Average();
+        }
+
+
+        private class TimeCalculator
+        {
+            Stopwatch _stopWatch { get; set; }
+
+            public TimeCalculator()
+            {
+                if (!Stopwatch.IsHighResolution)
+                    throw new NotSupportedException("Your hardware doesn't support high resolution counter");
+
+                long seed = Environment.TickCount;
+                Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(2);
+                Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+                Thread.CurrentThread.Priority = ThreadPriority.Highest;
+                _stopWatch = new Stopwatch();
+            }
+
+            public void Start()
+            {
+                _stopWatch.Start();
+            }
+
+            public void Stop()
+            {
+                _stopWatch.Stop();
+            }
+
+            public TimeSpan GetElapsedTime()
+            {
+                return _stopWatch.Elapsed;
+            }
+
+            public void Reset()
+            {
+                _stopWatch.Reset();
+            }
+        }
+    }
+}
