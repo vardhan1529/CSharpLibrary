f33f9d0dd8c1a27bead052558279fff707e275a3
Jayavardhan <vardhan1529@gmail.com>
2017-10-13 15:11:49 +0530

    Very Simple Factory, Observer Patterns template and goal calculation template

---
+++ CSharpLibrary/CSharpLibrary/ConceptSamples/ObseverPattern.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary.ConceptSamples
+{
+    class ObseverPattern
+    {
+        internal interface IUser
+        {
+            Guid Id { get; set; }
+            void Update();
+        }
+
+        internal interface IShoeRetailer<T>
+        {
+            void SubscribeUser(T user);
+            void UnSubscribeUser(Guid userId);
+            void NotifyUsers();           
+        }
+
+        internal class User : IUser
+        {
+            public Guid Id { get; set; }
+            public void Update()
+            {
+            }
+        }
+
+        internal class ShoeRetiler<T>: IShoeRetailer<T> where T:IUser
+        {
+            List<T> UsersList { get; set; }
+            public void SubscribeUser(T user)
+            {
+                UsersList.Add(user);
+            }
+            public void UnSubscribeUser(Guid userId)
+            {
+                var user = UsersList.Find(m => m.Id == userId);
+                if(user != null)
+                {
+                    UsersList.Remove(user);
+                }
+            }
+            public void NotifyUsers()
+            {
+                foreach(var user in UsersList)
+                {
+                    user.Update();
+                }
+            }
+            public void AddCampaign()
+            {
+                NotifyUsers();
+            }
+        }
+    }
+}
---
+++ CSharpLibrary/CSharpLibrary/ConceptSamples/SampleGoalCalculationArc.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary.ConceptSamples
+{
+    class SampleGoalCalculationArc
+    {
+        class GoalRequest
+        {
+            public Guid Id { get; set; }
+            public string Type { get; set; }
+            public string Calculation { get; set; }
+            public string AdjustmentFactor { get; set; }
+        }
+
+        class GoalResponse
+        {
+            public double Value { get; set; }
+        }
+
+        enum ErrorType
+        {
+            NotFound,
+            BadRequest,
+            InternalServerError,
+            NullReferenceException,
+            ValidationError
+        }
+
+        class ErrorResponse
+        {
+            public ErrorType Type { get; set; }
+            public string Message { get; set; }
+        }
+
+        class CalculatedGoalResponse
+        {
+            public Guid Id { get; set; }
+            public bool Success { get; set; }
+            public ErrorResponse Error { get; set; }
+            public GoalResponse Goal { get; set; }
+        }
+        class Main
+        {
+            public CalculatedGoalResponse CalculateGoal(GoalRequest request)
+            {
+                return GetCalclulator(request.Type).Execute(request);
+            }
+
+            private Calculator GetCalclulator(string type)
+            {
+                // create an object based on the type
+                return null;
+            }
+        }
+
+        abstract class Calculator
+        {
+            protected GoalRequest Request { get; set; }
+            protected abstract bool ValidateRequest();
+            protected abstract double CalculateGoalValue();
+            protected abstract double AdjustCalculatedGoalValue(double calculatedValue);
+            public virtual CalculatedGoalResponse Execute(GoalRequest request)
+            {
+                Request = request;
+                var response = new CalculatedGoalResponse() { Id = Request.Id };
+                if (ValidateRequest())
+                {
+                    var calculatedGoalValue = CalculateGoalValue();
+                    var adjustedValue = AdjustCalculatedGoalValue(calculatedGoalValue);
+                    response.Success = true;
+                    response.Goal = new GoalResponse { Value = adjustedValue };
+                    return response;
+                }
+
+                response.Error = new ErrorResponse() { Message = "Validation failed", Type = ErrorType.ValidationError };
+                return response;
+            }
+        }
+
+        class CalculatorA : Calculator
+        {
+            public override CalculatedGoalResponse Execute(GoalRequest request)
+            {
+                return base.Execute(request);
+            }
+
+            protected override bool ValidateRequest()
+            {
+                //Validation check
+                return true;
+            }
+
+            protected override double AdjustCalculatedGoalValue(double calculatedValue)
+            {
+                //Adjust the calculated value based on the adjustment factor in the request
+                return calculatedValue;
+            }
+
+            protected override double CalculateGoalValue()
+            {
+                //Calculate goal value
+                return 1;
+            }
+        }
+    }
+}
