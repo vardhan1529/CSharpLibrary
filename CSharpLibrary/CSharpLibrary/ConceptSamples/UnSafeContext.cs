2a1e25244bb0b50ac6e163252a7580f04f26f061
Jayavardhan <vardhan1529@gmail.com>
2017-10-13 18:15:57 +0530

    Unsafe and simple string iterations

---
+++ CSharpLibrary/CSharpLibrary/ConceptSamples/UnSafeContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary.ConceptSamples
+{
+    /*
+    Any of the following types may be a pointer type:
+    sbyte, byte, short, ushort, int, uint, long, ulong, char, float, double, decimal, or bool.
+    Any enum type.
+    Any pointer type.
+    Any user-defined struct type that contains fields of unmanaged types only.
+    */
+    class UnSafeContext
+    {
+        void InlineUnsafe()
+        {
+            unsafe
+            {
+                char p = 'V';
+                char* pp = &p;
+            }
+        }
+
+        unsafe void MethodLevelUnsafe()
+        {
+            int i = 90;
+            int* ip = &i;
+        }
+    }
+}
---
+++ CSharpLibrary/CSharpLibrary/Utility/StringManipulations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary.Utility
+{
+    static class StringManipulations
+    {
+        public static void  Iterate(this string data)
+        {
+            foreach(var d in data)
+            {
+                Console.Write(d);
+            }
+
+            Console.WriteLine();
+
+            for(var i=0;i<data.Length; i++)
+            {
+                Console.Write(data[i]);
+            }
+
+            Console.WriteLine();
+
+            var de = data.GetEnumerator();
+            while(de.MoveNext())
+            {
+                Console.Write(de.Current);
+            }
+
+            Console.WriteLine();
+
+            var da = data.ToArray();
+            foreach(var d in da)
+            {
+                Console.Write(d);
+            }
+        }
+    }
+}
